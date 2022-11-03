47529f6f192664ba6e40a3039e9827e08652ae7c
Oguz Yilmaz <oguz.yilmaz@yahoo.com>
2022-11-03 23:59:47 +0300

    Adds husky pre-commit hooks

--- rollup.config.js
+++ rollup.config.js
@@ -1,34 +1,34 @@
-import json from '@rollup/plugin-json';
-import babel from '@rollup/plugin-babel';
-import { terser } from 'rollup-plugin-terser';
-import resolve from '@rollup/plugin-node-resolve';
-import commonjs from '@rollup/plugin-commonjs';
+import json from '@rollup/plugin-json'
+import babel from '@rollup/plugin-babel'
+import { terser } from 'rollup-plugin-terser'
+import resolve from '@rollup/plugin-node-resolve'
+import commonjs from '@rollup/plugin-commonjs'
 
 const pkg = {
     name: 'Generatorjs',
-    format: 'umd',
-};
+    format: 'umd'
+}
 
 export default {
-    input: 'src/index.js',
+    input: 'src/index.ts',
     output: [
         {
             ...pkg,
-            file: 'dist/Generator.js',
+            file: 'dist/Generator.js'
         },
         {
             ...pkg,
             file: 'dist/Generator.min.js',
-            plugins: [terser()], // minifies
-        },
+            plugins: [terser()] // minifies
+        }
     ],
     plugins: [
         json(), // converts .json files to ES6 modules.
         resolve(),
         commonjs(),
         babel({
-            exclude: 'node_modules/**', // only transpile our source code
-        }),
+            exclude: 'node_modules/**' // only transpile our source code
+        })
     ],
-    context: 'window',
-};
+    context: 'window'
+}
