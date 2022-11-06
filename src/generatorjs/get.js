9537479c5f17bd12be91cfe135a805a37ac714e4
Oguz Yilmaz <oguz.yilmaz@yahoo.com>
2022-11-06 18:56:00 +0300

    Fix build

--- src/generatorjs/get.js
+++ src/generatorjs/get.js
@@ -1,6 +1,7 @@
 import { TAG_NAMES } from '../constants'
 import { stringStarts, isString } from '../utils'
-import { selectElement, createElement } from '../dom-utils'
+import { createElement } from '../dom-utils'
+import selectElement from './selectElement'
 
 export default function get() {
     const len = arguments.length
@@ -9,44 +10,43 @@ export default function get() {
 
     if (len > 3) {
         throw new TypeError(
-            'Wrong number of argument supplied to Generatorjs.get function.Max 3 allowed based on usage. ',
+            'Wrong number of argument supplied to Generatorjs.get function.Max 3 allowed based on usage. '
         )
-    }
-    else if (len === 0) {
+    } else if (len === 0) {
         return this.$fragment
-    }
-    else if (len === 1) {
+    } else if (len === 1) {
         fragment = this.$fragment.cloneNode(true)
         const elem = arguments[0]
 
         if (!isString(elem)) {
-            throw new TypeError(`Argument passed should be a string. ${typeof elem} is given!`)
+            throw new TypeError(
+                `Argument passed should be a string. ${typeof elem} is given!`
+            )
         }
 
         if (stringStarts(elem, '.')) {
             currentElement = selectElement.call(this, fragment, elem, 'class')
-        }
-        else if (stringStarts(elem, '#')) {
+        } else if (stringStarts(elem, '#')) {
             currentElement = selectElement.call(this, fragment, elem, 'id')
-        }
-        else if (stringStarts(elem, 'name=')) {
+        } else if (stringStarts(elem, 'name=')) {
             const name = elem.split('=')[1]
             currentElement = selectElement.call(this, fragment, name, 'name')
             // for attribute selector
             // JQuery jQuery( "[attribute='value']" )
-        }
-        else if (stringStarts(elem, '[') || elem.indexOf('[') !== -1) {
+        } else if (stringStarts(elem, '[') || elem.indexOf('[') !== -1) {
             currentElement = selectElement.call(this, fragment, elem, 'attr')
-        }
-        else if (elem === '*') {
+        } else if (elem === '*') {
             const div = createElement('div')
             div.appendChild(fragment)
             currentElement = div.firstChild
-        }
-        else if (TAG_NAMES.indexOf(elem.toLowerCase())) {
-            currentElement = selectElement.call(this, fragment, elem.toLowerCase(), 'tag')
-        }
-        else {
+        } else if (TAG_NAMES.indexOf(elem.toLowerCase())) {
+            currentElement = selectElement.call(
+                this,
+                fragment,
+                elem.toLowerCase(),
+                'tag'
+            )
+        } else {
             // here is a special Generatorjs selector
             // .get('p 4')
             // currentElement = this.selector(elem);
