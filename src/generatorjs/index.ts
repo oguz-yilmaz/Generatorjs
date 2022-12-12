fb5a48d4a3536c783691883a165624aeecb2f763
Oguz Yilmaz <oguz.yilmaz@yahoo.com>
2022-12-12 23:20:32 +0300

    Updates imports

--- src/generatorjs/index.ts
+++ src/generatorjs/index.ts
@@ -4,5 +4,15 @@ import attachTo from './attachTo'
 import deleteEvent from './deleteEvent'
 import reset from './reset'
 import setContent from './setContent'
+import { select, selectAll } from './selects'
 
-export { addEvent, append, attachTo, deleteEvent, reset, setContent }
+export {
+    addEvent,
+    append,
+    attachTo,
+    deleteEvent,
+    reset,
+    setContent,
+    select,
+    selectAll
+}
