cc0def9011ffaded3868e8ab7d74744b3a067aca
Oguz Yilmaz <oguz.yilmaz@yahoo.com>
2022-12-21 21:40:51 +0300

    Removes unused utils

--- src/utils.ts
+++ src/utils.ts
@@ -6,7 +6,7 @@
 export const isPlainObject = (obj) =>
     Object.prototype.toString.call(obj) === '[object Object]'
 
-export const isString = (value) => {
+export const isString = (value): value is string => {
     if (typeof value === 'string') {
         return true
     }
@@ -20,15 +20,8 @@ export const isFunction = (value) =>
 
 export const isDef = (v) => v !== undefined && v !== null
 
-export const isObject = (value) => Object(value) === value
-
 export const emptyArray = (array) =>
     typeof array !== 'undefined' && array !== null && array.length <= 0
-
-export const hasOwn = (
-    () => (obj, prop) =>
-        Object.prototype.hasOwnProperty.call(obj, prop)
-)()
 
 export const forEach = (iterable, callback) => {
     if (Array.isArray(iterable)) {
@@ -40,31 +33,6 @@ export const forEach = (iterable, callback) => {
             callback(value[0], value[1])
         )
     }
-}
-
-export const isNodeList = (node) => {
-    const stringRepr = Object.prototype.toString.call(node)
-
-    let res =
-        typeof node === 'object' &&
-        /^\[object (HTMLCollection|NodeList|Object)\]$/.test(stringRepr) &&
-        typeof node.length === 'number' &&
-        (node.length === 0 ||
-            (typeof node[0] === 'object' && node[0].nodeType > 0))
-
-    if (res === false) {
-        // try for IE
-        if (
-            typeof node.length === 'number' &&
-            typeof node.item !== 'undefined' &&
-            typeof node.nextNode === 'function' &&
-            typeof node.reset === 'function'
-        ) {
-            res = true
-        }
-    }
-
-    return res
 }
 
 // Cross-browser means of setting innerHTML on a DOM Element.
